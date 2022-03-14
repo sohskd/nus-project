c6031462c14caf90d8a0d94fd219838336537fe7
desmond.sohsk@shopee.com <desmond.sohsk@shopee.com>
2022-03-14 15:22:32 +0800

    :sparkles: add controller

--- back-end/project-capstone/03-nus-order-matching-service/03-order-matching-service/src/main/java/com/nus/team3/OrderMatchingServceApplication.java
+++ back-end/project-capstone/03-nus-order-matching-service/03-order-matching-service/src/main/java/com/nus/team3/OrderMatchingServceApplication.java
@@ -2,9 +2,19 @@ package com.nus.team3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class OrderMatchingServceApplication {
+
+  @RequestMapping("/")
+  @ResponseBody
+  String home() {
+    return "Hello World Order!";
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(OrderMatchingServceApplication.class, args);
