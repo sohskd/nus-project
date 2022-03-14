d2b8d5652cb95588342043f19aba2af5c6abc65a
desmond.sohsk@shopee.com <desmond.sohsk@shopee.com>
2022-03-14 09:59:21 +0800

    :sparkles: add backend

---
+++ back-end/project-capstone/src/test/java/com/nus/team3/projectcapstone/ProjectCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nus.team3.projectcapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
