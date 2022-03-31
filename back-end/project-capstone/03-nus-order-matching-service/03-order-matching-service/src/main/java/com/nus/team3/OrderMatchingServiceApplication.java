0147f5f46e69a155ef4280a82e3a7ba53ecb15bb
Siyuan <jilingou@gmail.com>
2022-03-31 16:36:39 +0800

    minor fix

--- back-end/project-capstone/03-nus-order-matching-service/03-order-matching-service/src/main/java/com/nus/team3/OrderMatchingServiceApplication.java
+++ back-end/project-capstone/03-nus-order-matching-service/03-order-matching-service/src/main/java/com/nus/team3/OrderMatchingServiceApplication.java
@@ -4,10 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Controller
+@EnableSwagger2
 @SpringBootApplication(exclude = { ContextStackAutoConfiguration.class })
-@ComponentScan(basePackages = "com.nus.team3.*")
 public class OrderMatchingServiceApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World Order!";
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(OrderMatchingServiceApplication.class, args);
     }
