f52f396be6ee6dcb885714f205016a87dfc3e2f1
mnyue <liumigyue@gmail.com>
2022-04-21 21:16:29 +0800

    Account Management development

--- back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/dto/User.java
+++ back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/dto/User.java
@@ -15,6 +15,59 @@ public class User {
 
   private String email;
 
-  private String name;
+  private String username;
 
- }
+  private String password;
+
+  private Boolean loggon_i;  
+
+ 
+
+  public Integer getId(){
+   return id;
+  }
+
+  public String getEmail(){
+    return email;
+  }
+  
+  public String getUsername(){
+    return username;
+  }
+
+  public String getPassword(){
+    return password;
+  }
+
+  public Boolean getLoggon_i(){
+    return loggon_i;
+  }
+
+  public void setId(Integer id){
+    this.id = id;
+  }
+  
+  public void setEmail(String email){
+     this.email = email;
+  }
+  
+  public void setUsername(String username){
+    this.username = username;
+  }
+  
+  public void setPassword(String password){
+    this.password = password;
+  }
+
+  public void setLoggon_i(Boolean loggon_i){
+    this.loggon_i = loggon_i;
+  }
+}
+
+
+
+
+
+
+
+
