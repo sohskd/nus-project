21587ab30f0fd8d20469e961fb5cd7ae01e097a9
Soh Thiam Hing <daffodilistic@gmail.com>
2022-04-27 00:01:35 +0800

    [fix] Use Spring Data JPA to automatically increment IDs when creating users

--- back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/dto/User.java
+++ back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/dto/User.java
@@ -4,24 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class User {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
-
   private String email;
-
   private String username;
-
   private String password;
-
   private Boolean loggon_i;  
 
- 
 
   public Integer getId(){
    return id;
--- back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/mapper/AccountServiceMapper.java
+++ back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/mapper/AccountServiceMapper.java
@@ -12,30 +12,30 @@ import org.springframework.stereotype.Component;
 @Component
 public interface AccountServiceMapper {
     class Statements {
-        static final String USER_LOGIN = "UPDATE user_account_tab " +
+        static final String USER_GET = "SELECT * FROM user WHERE id=#{id}";
+        static final String USER_CREATE = "INSERT INTO user (email,username,password)" +
+                "VALUES (#{email}, #{username}, #{password})";
+        static final String USER_LOGIN = "UPDATE user " +
                 "SET loggon_i = 1 " +
                 "WHERE username = #{username} ";
-        static final String USER_LOGOFF = "UPDATE user_account_tab" +
+        static final String USER_LOGOFF = "UPDATE user" +
                 "SET loggon_i = 0" +
                 "WHERE username = #{username}";
     }
 
-    @Select({ "SELECT * FROM user_account_tab WHERE id=#{id}" })
+    @Select({ Statements.USER_GET })
     @Results({
             @Result(property = "id", column = "id"),
             @Result(property = "email", column = "email"),
-            @Result(property = "name", column = "name"),
+            @Result(property = "username", column = "username"),
             @Result(property = "password", column = "password"),
             @Result(property = "loggon_i", column = "loggon_i"),
 
     })
     List<User> getUserInfo(Integer id);
 
-    @Insert("Insert into user_account_tab (id,email,name,password)" +
-            "values (#{id}," +
-            "#{email}," +
-            "#{name}," +
-            "#{password})")
+    @Options(useGeneratedKeys=true, keyProperty="id")
+    @Insert({ Statements.USER_CREATE })
     public void createNewAccount(User user);
 
     @Update({ Statements.USER_LOGIN })
