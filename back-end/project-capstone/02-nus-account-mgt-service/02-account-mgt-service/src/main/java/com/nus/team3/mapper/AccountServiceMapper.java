50099a7e38f915745a979e5d1f2c5f26ec251e94
Soh Thiam Hing <daffodilistic@gmail.com>
2022-04-26 22:54:04 +0800

    [fix] Fix exception thrown about invalid XML for login and logout methods

--- back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/mapper/AccountServiceMapper.java
+++ back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/mapper/AccountServiceMapper.java
@@ -11,51 +11,37 @@ import org.springframework.stereotype.Component;
 
 @Component
 public interface AccountServiceMapper {
-    @Select({"SELECT * FROM user_account_tab WHERE id=#{id}"})
+    class Statements {
+        static final String USER_LOGIN = "UPDATE user_account_tab " +
+                "SET loggon_i = 1 " +
+                "WHERE username = #{username} ";
+        static final String USER_LOGOFF = "UPDATE user_account_tab" +
+                "SET loggon_i = 0" +
+                "WHERE username = #{username}";
+    }
+
+    @Select({ "SELECT * FROM user_account_tab WHERE id=#{id}" })
     @Results({
             @Result(property = "id", column = "id"),
             @Result(property = "email", column = "email"),
             @Result(property = "name", column = "name"),
             @Result(property = "password", column = "password"),
             @Result(property = "loggon_i", column = "loggon_i"),
-           
+
     })
     List<User> getUserInfo(Integer id);
 
     @Insert("Insert into user_account_tab (id,email,name,password)" +
-           "values (#{id}," +
-           "#{email}," +
-           "#{name}," +
-           "#{password})")
+            "values (#{id}," +
+            "#{email}," +
+            "#{name}," +
+            "#{password})")
     public void createNewAccount(User user);
-     
-    @Update({
-            "<script>",
-            "UPDATE user_account_tab",
-            "<set>",
-            "<loggon_i = 1>",
-            "</set>",
-            "</set>",
-            "<where>",
-            "username =#{username}",
-            "</where>",
-            "</script>"
 
-    })
-    void userLogon (@Param("username") boolean loggon_i);
+    @Update({ Statements.USER_LOGIN })
+    void userLogon(@Param("username") boolean loggon_i);
 
-    @Update({
-            "<script>",
-            "UPDATE user_account_tab",
-            "<set>",
-            "<loggon_i = 0>",
-            "</set>",
-            "<where>",
-            "username =#{username}",
-            "</where>",
-            "</script>"
-
-    })
-    void userLogoff (@Param("username") boolean loggon_i);
+    @Update({ Statements.USER_LOGOFF })
+    void userLogoff(@Param("username") boolean loggon_i);
 
 }
