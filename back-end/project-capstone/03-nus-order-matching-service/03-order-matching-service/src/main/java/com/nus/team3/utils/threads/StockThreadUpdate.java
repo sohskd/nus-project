99ede7af32c226c49475ccb60ab999f0559cba79
Soh Thiam Hing <daffodilistic@gmail.com>
2022-04-30 09:38:56 +0800

    [ci/cd] remove lombok.var import as Java 11 supports the var keyword natively

--- back-end/project-capstone/03-nus-order-matching-service/03-order-matching-service/src/main/java/com/nus/team3/utils/threads/StockThreadUpdate.java
+++ back-end/project-capstone/03-nus-order-matching-service/03-order-matching-service/src/main/java/com/nus/team3/utils/threads/StockThreadUpdate.java
@@ -9,7 +9,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
-import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
