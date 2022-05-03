92e3a592100868210cf7e38c8cdbc23fbc677014
Soh Thiam Hing <daffodilistic@gmail.com>
2022-05-03 16:19:02 +0800

    [chore] whoops, forgot to include LoginView

---
+++ front-end/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    <h1>This is the login page</h1>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "../components/HelloWorld";
+
+export default {
+  name: "LoginView",
+
+  components: {
+    // HelloWorld,
+  },
+};
+</script>
