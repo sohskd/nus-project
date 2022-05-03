955f737041eebb2bb8739db2f19e01e52e9a27ed
Soh Thiam Hing <daffodilistic@gmail.com>
2022-05-03 20:49:00 +0800

    [fix] Allow CORS requests from *.omni-trade.xyz

--- back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/dao/AccountDao.java
+++ back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/dao/AccountDao.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpSession;
 
 import com.nus.team3.dto.User;
 
+@CrossOrigin(origins = {"*.omni-trade.xyz"})
 @RestController
 @RequestMapping("/account")
 public class AccountDao {
