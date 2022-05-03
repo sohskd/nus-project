c6b6e894bf1a5b3b4a83907758b13bac89af4af9
Soh Thiam Hing <daffodilistic@gmail.com>
2022-05-03 16:06:29 +0800

    [feature] Set default title for web app

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      title: 'Omni Trade',
+    },
+  }
 });
