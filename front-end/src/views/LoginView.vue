2c1d7e7003a6e8dc2c8799a3d796482cbd546185
Soh Thiam Hing <daffodilistic@gmail.com>
2022-05-03 17:09:39 +0800

    [feature] Finish UI for login

--- front-end/src/views/LoginView.vue
+++ front-end/src/views/LoginView.vue
@@ -1,7 +1,34 @@
 <template>
-  <div class="login">
-    <h1>This is the login page</h1>
-  </div>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-col>
+        <v-card class="mx-auto" max-width="344">
+          <v-card-text>
+            <p class="text-h4 text--primary">Login</p>
+            <v-text-field
+              label="Username"
+              prepend-inner-icon="mdi-account"
+              dense
+              filled
+            ></v-text-field>
+            <v-text-field
+              label="Password"
+              type="password"
+              prepend-inner-icon="mdi-key"
+              dense
+              filled
+            ></v-text-field>
+            <v-divider />
+          </v-card-text>
+          <v-card-actions>
+            <v-col class="text-right">
+              <v-btn color="teal accent-4" dark @click="login">Login</v-btn>
+            </v-col>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -12,6 +39,12 @@ export default {
 
   components: {
     // HelloWorld,
+  },
+
+  methods: {
+    login() {
+      console.log("LOGIN");
+    },
   },
 };
 </script>
