35ae4361cb8f0984e96db210d6524a793ba9a978
Soh Thiam Hing <daffodilistic@gmail.com>
2022-05-04 16:42:53 +0800

    [feature(dashboard)] Initial work on dashboard

--- front-end/src/views/DashboardView.vue
+++ front-end/src/views/DashboardView.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="about">
-    <h1>This is the dashboard view</h1>
+    <v-container fluid>
+      <v-row dense>
+        <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    cards: [
+      {
+        src: "https://via.placeholder.com/500x500/000000/FFFFFF/?text=SE",
+        flex: 6,
+      },
+      {
+        src: "https://via.placeholder.com/500x500/000000/FFFFFF/?text=MSFT",
+        flex: 6,
+      },
+    ],
+  }),
+};
+</script>
