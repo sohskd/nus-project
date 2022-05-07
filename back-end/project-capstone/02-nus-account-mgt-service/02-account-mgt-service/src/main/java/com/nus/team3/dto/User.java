5a547338669336e62c14716542a16d46eea66667
Soh Thiam Hing <daffodilistic@gmail.com>
2022-05-07 20:22:08 +0800

    [fix] Add missing ID prop, which is needed for Hibernate ORM to auto-create the table for us

--- back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/dto/User.java
+++ back-end/project-capstone/02-nus-account-mgt-service/02-account-mgt-service/src/main/java/com/nus/team3/dto/User.java
@@ -20,7 +20,7 @@ public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  
+  private long id;
   private String email;
   private String username;
   private String password;
