954f98dce1beb8e8fa77cd9811aa18af578061dd
Soh Thiam Hing <daffodilistic@gmail.com>
2022-05-07 14:55:39 +0800

    [experimental] Working hardcoded configuration to connect to ElasticMQ

--- back-end/project-capstone/03-nus-order-matching-service/03-order-matching-service/src/main/java/com/nus/team3/config/SQSConfig.java
+++ back-end/project-capstone/03-nus-order-matching-service/03-order-matching-service/src/main/java/com/nus/team3/config/SQSConfig.java
@@ -1,44 +1,40 @@
 package com.nus.team3.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class SQSConfig {
-	@Value("${cloud.aws.region.static}")
+	// @Value("${cloud.aws.region.static}")
 	private String region;
 
-	@Value("${cloud.aws.credentials.access-key}")
+	// @Value("${cloud.aws.credentials.access-key}")
 	private String accessKey;
 
-	@Value("${cloud.aws.credentials.secret-key}")
+	// @Value("${cloud.aws.credentials.secret-key}")
 	private String secretKey;
 
+	// @Bean
+	// public QueueMessagingTemplate queueMessagingTemplate() {
+	// 	return new QueueMessagingTemplate(amazonSQSAsync());
+	// }
+
 	@Bean
-	public QueueMessagingTemplate queueMessagingTemplate() {
-		return new QueueMessagingTemplate(amazonSQSAsync());
-	}
-
 	public AmazonSQSAsync amazonSQSAsync() {
-
-		AmazonSQSAsyncClientBuilder amazonSQSAsyncClientBuilder = AmazonSQSAsyncClientBuilder.standard();
-		AmazonSQSAsync amazonSQSAsync = null;
-		// amazonSQSAsyncClientBuilder.withRegion(Regions.AP_SOUTHEAST_2);
 		BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials("x", "x");
-		amazonSQSAsyncClientBuilder.withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials));
-		amazonSQSAsyncClientBuilder.withEndpointConfiguration(
-				new AmazonSQSAsyncClientBuilder.EndpointConfiguration("localhost:9324", "ap-southeast-2"));
-		amazonSQSAsync = amazonSQSAsyncClientBuilder.build();
+		EndpointConfiguration endpointConfiguration = new AmazonSQSAsyncClientBuilder.EndpointConfiguration(
+				"http://localhost:9324",
+				"ap-southeast-2");
+		AmazonSQSAsyncClientBuilder amazonSQSAsyncClientBuilder = AmazonSQSAsyncClientBuilder.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+				.withEndpointConfiguration(endpointConfiguration);
+		AmazonSQSAsync amazonSQSAsync = amazonSQSAsyncClientBuilder.build();
 		return amazonSQSAsync;
-
 	}
 
 }
